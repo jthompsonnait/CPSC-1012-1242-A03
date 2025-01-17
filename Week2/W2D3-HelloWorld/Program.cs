509b570ffe50e29e2ec852b564356b0a7bf42457
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2025-01-17 11:34:26 -0700

    Week 2 Day 3 - Hello World
    Week 2 Day 3 - Hello World

---
+++ Week2/W2D3-HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace W2D3_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Goodbye, World!");
+        }
+    }
+}
