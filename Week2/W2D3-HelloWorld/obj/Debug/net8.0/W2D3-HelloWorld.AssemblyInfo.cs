ebd801d189d2cff9d807e676b6dd5564bd18b56e
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2025-01-20 10:08:29 -0700

    Beginning of class

--- Week2/W2D3-HelloWorld/obj/Debug/net8.0/W2D3-HelloWorld.AssemblyInfo.cs
+++ Week2/W2D3-HelloWorld/obj/Debug/net8.0/W2D3-HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("W2D3-HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6bd22653a9a3e991731e169fa232c691ecc6637")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be8997fbced703d2499843cf6429767dbf3725b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("W2D3-HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("W2D3-HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
