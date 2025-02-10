1a814b25b9ebb5e85ebfdcc6401d78f4608b0d60
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2025-02-10 11:52:05 -0700

    End of class

---
+++ CPSC1012/W6D1-Loop1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace W6D1_Loop1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int total = 0;
+            Console.Write("Enter a number to square and add: ");
+            number = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= number; i++)
+            {
+                total = total + (i * i);
+            }
+            Console.WriteLine($"Your answer is {total}");
+
+            //  while
+            int whileNumber = number;
+            total = 0;
+            while (whileNumber > 0)
+            {
+                total = total + (whileNumber * whileNumber);
+                whileNumber = whileNumber - 1;
+            }
+            Console.WriteLine($"While - Your answer is {total}");
+
+            //  do while
+            int doWhileNumber = number;
+            total = 0;
+            do
+            {
+                total = total + (doWhileNumber * doWhileNumber);
+                doWhileNumber = doWhileNumber - 1;
+            } while (doWhileNumber > 0);
+           
+            Console.WriteLine($"While - Your answer is {total}");
+        }
+    }
+}
---
+++ CPSC1012/W6D3-Total/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace W6D3_Total
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int num = 0;
+
+            do
+            {
+                Console.Write("Enter numbers to add (0 to stop): ");
+                num = int.Parse(Console.ReadLine());
+                //sum += num;
+                sum = sum + num;
+            } while (num != 0);
+
+            Console.WriteLine($"Total sum: {sum}");
+        }
+    }
+}
