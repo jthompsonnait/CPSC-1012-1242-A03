612a4677996f20222a6f9878da146184f692b824
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2025-02-12 11:52:01 -0700

    End of Class Week 6 Day 2

---
+++ CPSC1012/W6D2-CounterBasic/Program.cs
@@ -0,0 +1,114 @@
+﻿namespace W6D2_CounterBasic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  adding a new line for a string
+            Console.Write("Line 1\nLine2\n");
+            //  adding a carriage return for a string
+            Console.Write("Line 1\r\nLine2\r\n");
+            //  adding a carriage return using environment property
+            Console.Write($"Line 1{Environment.NewLine}Line2{Environment.NewLine}");
+
+            //  For Loop
+            //  basic pattern for counter control loop
+            //  1) Initialization
+            //  2) Condition
+            //  3) Increment or decrement
+            //  for (initialization; condition; increment or decrement)
+            //  {...}
+
+            //  simple for loop
+            Console.WriteLine("Simple for loop");
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //  add 2 blanks lines
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //  count down loop
+            Console.WriteLine("Count down loop");
+            for (int i = 5; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            //  add 2 blanks lines
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //  skipping numbers
+            Console.WriteLine("Skipping numbers");
+            for (int i = 1; i <= 20; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            //  Common Pitt falls
+            //  Infinite loop
+            //  Inside our for loop, we do have a valid condition to terminate our loop
+            //for (int i = 1; i > 0; i++)
+            //{
+            //    Console.WriteLine($"This loop will run forever ! Current number is {i}");
+            //}
+
+            //  Incorrect Initialization
+            //  Improperly initializing the loop control variable that can't execute
+            //      as expected
+            for (int i = 10; i < 5; i++)
+            {
+                Console.WriteLine("This loop will not run");
+            }
+
+            //  Forgetting to Increment/Decrement
+            //  Forgetting to update the loop control variable
+            //for (int i = 1; i <= 5; )
+            //{
+            //    Console.WriteLine($"Number: {i}");
+            //}
+
+            //  Off by one error
+            //  When a loop iterates one too many or one too few times
+            //  We only want numbers between 0 - 4
+            //  for (int i = 0; i < 5; i++)
+            //  for (int i = 0; i <= 4; i++)
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine($"Number: {i}");
+            }
+
+            //  Not initialization at the right point
+            //  This is an issue where your initialization is not started at the
+            //      correct location or value
+            //  Example.  Enter in your name and print out each character on a
+            //              separate line
+            //  Starting at the wrong value
+            //      for (int i = 1; i < name.Length; i++) // Start @ 1 and not 0
+            //      for (int i = 0; i <= name.Length; i++) // loop control variable is out of range
+            //Console.Write("Please enter in your name: ");
+            //string name = Console.ReadLine();
+
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //  Inner and outer loop
+            //  A loop within a loop
+            //  outer loop
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"Outer Loop: {i}");
+                //  inner loop
+                for (int j = 105; j > 100; j--)
+                {
+                    Console.WriteLine($"Inner Loop: {j}");
+                }
+            }
+        }
+    }
+}
---
+++ CPSC1012/W6D2-GuessingGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace W6D2_GuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  create a game to ask for a number between 1 and 100
+            //  create a random number 
+            //  If the random number is lower than the user input (guess),
+            //      then we will tell the user that their number is high
+            //  If the random number is higher than the user input (guess),
+            //      then we will tell the user that their number is low
+            //  If the user guess the right answer, we will let them know
+
+            //  random object
+            Random random = new Random();
+            int number = random.Next(1, 101);
+            Console.WriteLine(number);
+            int userInput = 0;
+            do
+            {
+                Console.Write("Input a number between 1 and 100.  Enter 0 to Quit: ");
+                userInput = int.Parse(Console.ReadLine());
+                if (number < userInput)
+                {
+                    Console.WriteLine("Your number is too high");
+                }
+                else if (number > userInput)
+                {
+                    Console.WriteLine("Your number is too high");
+                }
+            } while (number != userInput && userInput != 0);
+
+            Console.WriteLine("You got it right!!!!");
+        }
+    }
+}
