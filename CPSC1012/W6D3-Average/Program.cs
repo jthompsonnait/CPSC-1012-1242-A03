ef1d218083ad706fc3b692867253d9feb99a447c
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2025-02-14 11:47:55 -0700

    End of Class

---
+++ CPSC1012/W6D3-Average/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Numerics;
+
+namespace W6D3_Average
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Purpose: Allow the user to enter positive numbers until they enter zero.
+        /// Display the average of the numbers entered.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            //  user input number
+            int number = 0;
+            //  average of our number
+            double average;
+            //  count
+            int count = 0;
+            //  running total
+            int runningTotal = 0;
+
+            Console.WriteLine("==== Enter in a whole number until zero ====");
+            Console.WriteLine();
+
+            do
+            {
+                try
+                {
+                    Console.Write("Enter a positive number, zero to exit: ");
+                    // number  = int.Parse(Console.ReadLine());
+                    bool result =int.TryParse(Console.ReadLine(), out number);
+
+                    if (number < 0)
+                    {
+                        throw new Exception("You can't have a negative value");
+                    }
+                }              
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"You must enter a positive value - {ex.Message}");
+                    number = -1;
+                }
+                // if we have a positive number
+                if(number > 0)
+                {
+                    // sum our running total
+                    runningTotal = runningTotal + number;  // runningTotal += number;
+                    //  increase our count by 1
+                    count = count + 1;  // count++;  count +=1;
+                    
+                }
+
+            } while (number != 0);
+
+            if (count > 0)
+            {
+                average = runningTotal / count;
+                Console.WriteLine();
+                Console.WriteLine($"The average is {average}.");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("===== End Program =====");
+        }
+    }
+}
---
+++ CPSC1012/W6D3-ExceptionHandling/Program.cs
@@ -0,0 +1,167 @@
+﻿namespace W6D3_ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Computer programs, if not coded properly or with the correct safeguards,
+            // may throw what are called Runtime Exceptions.
+            // These are errors that occur while the
+            // program is running ("run time")
+
+            // An exception is a type of computer error that occurs when a program is
+            // trying to do something, but can't.
+            // For example, we get an exception when we try an parse a sentence to an int
+
+            // int.Parse("HELLO");
+
+            // the above line will throw an exception because the program cannot convert
+            // "HELLO" to an integer.
+
+            // There are hundreds and hundreds of exceptions built into programming languages
+            // and we can even create our own.
+
+            // Here are some examples of exceptions and the multiple ways we can handle them.
+
+            /**** Divide by Zero Exception ****/
+
+            int i = 5;
+            int j = 0;
+
+            // If we run the following line, we will get a DivideByZeroException and the program will quit.
+            // (uncomment the line and see for yourself)
+
+            // Console.WriteLine(i / j);
+
+            // There are a couple ways we can avoid this problem.
+            // Using logic:
+
+            if (j != 0)
+            {
+                Console.WriteLine(i / j);
+            }
+            else
+            {
+                Console.WriteLine("Cannot divide by zero.");
+            }
+
+
+            // Using logic will work, but it may not always be the best choice for resolving the problem.
+            // The other way to do this is to use a Try/Catch to "catch" the exception
+
+            // A try/catch works in the following way. We place the code we want to run in the "try block"
+            // If an exception is thrown in the "try block", then all remaining code in the try block will
+            // be skipped, and the code in the "catch block" will execute instead.
+
+            // If an exception is not thrown, then the code in the catch block will not execute.
+
+            // Using the same "divide by zero" example:
+            try
+            {
+                //if this throws an exception, we will go to the catch block 
+                Console.WriteLine(i / j);
+            }
+            catch
+            {
+                // Execute our custom code when an exception is caught
+                Console.WriteLine("Cannot divide by zero.");
+            }
+
+
+            // One of the better parts of using a try/catch instead of logic to prevent exceptions
+            // is a try/catch can provide us with more information about the error.
+
+            try
+            {
+                Console.WriteLine(i / j);
+            }
+            catch (Exception ex)
+            {
+                // The Exception class can provide us with many options to help us diagnose the issue
+                // One of the most commonly used is ex.Message
+                Console.WriteLine("Failed to divide by zero.");
+
+                //ex.Message is a property that contains the error message. Very useful for logging!
+                Console.WriteLine(ex.Message);
+            }
+
+            /***** Parsing Exception *****/
+            // let's look at another example where we ask the user their age
+            // we will also place this inside of a while loop so that we can execute the same code over
+            // and over until a user enters a valid input.
+
+            int userAge;
+            bool validInput = false;
+
+            // Ask the user for their age
+            Console.Write("Enter your age: ");
+            while (!validInput)
+            {
+                try
+                {
+                    // if the user does not enter an integer, the exception will throw.
+                    userAge = int.Parse(Console.ReadLine());
+
+                    // If a user enters a negative number
+                    // we can throw our own exception because you can't be negative years old
+                    // using "throw" we force the code to enter the catch block
+                    // adding  "new Exception(customErrorMessage)" we can add our custom error message
+                    if (userAge < 0)
+                    {
+                        throw new Exception("You can't be negative years old.");
+                    }
+                    
+                    validInput = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    // If the user did not enter a valid age
+                    Console.WriteLine("Invalid Input. Please re-enter age: ");
+                }
+            }
+
+            /****** TryParse ******/
+            // Up until now, we have been using the Parse method to convert our string data to other
+            // data types. However, as we have seen, Parse opens us up to potential unhandled exceptions,
+            // which can cause problems in our programs.
+            // A more used way of parsing that can reduce the chance of this is the TryParse() method.
+
+            // TryParse actually returns a boolean value. If the parsing was successful, then the value of
+            // "true" is returned. If the parsing failed, then "false" will be returned.
+            // TryParse() takes two parameters, the first is the string to be parsed, the second is the variable that
+            // will hold the parsed value.
+            // Make sure to include the "out" keyword in the second parameter.
+
+            Console.Write("Enter a number again to test the TryParse method: ");
+            string input = Console.ReadLine();
+            int age; // declare the int variable so it can be used in the TryParse method
+
+            //                                  string       variable
+            //                                     |            |
+            //                                     |            |
+            //                                     V            V
+            bool parsingSuccessful = int.TryParse(input, out age);
+
+            // If the parsing is successful, the variable "userInt" will contain the parsed value.
+            if (parsingSuccessful)
+                Console.WriteLine($"Hooray, {age} was parsed and your age is {age}!");
+            else
+                Console.WriteLine($"Failed to parse {input} to an integer. Sad....");
+
+            // TryParse is a safer way of parsing strings as well as the preferred method.
+
+            //  Showing TryParse without a varaible to store the success/fail (parsingSuccessful)
+            Console.Write("Enter a second number again to test the TryParse method: ");
+            string input2 = Console.ReadLine();
+            int age2; // declare the int variable so it can be used in the TryParse method
+            
+            // If the parsing is successful, the variable "userInt" will contain the parsed value.
+            if (int.TryParse(input2, out age2))
+                Console.WriteLine($"Hooray, {age2} was parsed and your age is {age2}!");
+            else
+                Console.WriteLine($"Failed to parse {input2} to an integer. Sad....");
+        }
+    }
+}
+
