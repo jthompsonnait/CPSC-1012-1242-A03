2f2823341049d198af8b09f67d16d113a3244f9e
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2025-02-24 11:45:01 -0700

    End of class

---
+++ CPSC1012/W7D1-Exercise1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace W7D1_Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 0;
+            int[] values = new int[25];
+            bool cont = true;
+            double total = 0;
+            int count = 0;
+            do
+            {
+                Console.Write("Please enter in a number: ");
+                int.TryParse(Console.ReadLine(), out value);
+                if (value != 0 && count < 25)
+                {
+                    values[count] = value;
+                    count += 1;
+                }
+                else
+                {
+                    cont = false;
+                }
+            } while (cont);
+
+            foreach (var i in values)
+            {
+                total = total + i;
+            }
+            Console.WriteLine($"You enter {count} values and the average is {total / count}");
+        }
+    }
+}
