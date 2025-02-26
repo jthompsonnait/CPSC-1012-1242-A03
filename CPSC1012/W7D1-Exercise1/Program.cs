9cd1374b33793bf1592e896003a6476dadd8ca66
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2025-02-26 09:32:10 -0700

    Update exercise to use single For statement

--- CPSC1012/W7D1-Exercise1/Program.cs
+++ CPSC1012/W7D1-Exercise1/Program.cs
@@ -1,34 +1,66 @@
-﻿namespace W7D1_Exercise1
+﻿using System.Diagnostics.CodeAnalysis;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace W7D1_Exercise1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int value = 0;
-            int[] values = new int[25];
-            bool cont = true;
-            double total = 0;
+            //  array of numbers for storing our running numbers into.
+            int[] numbers = new int[25];
+            //  input value
+            int inputValue = 0;
+            //  current count of numbers enter
             int count = 0;
-            do
+            //  total of all numbers
+            double total = 0;
+            //  average (total/count) assuming count > 0
+            double average = 0;
+
+            //  process number
+            for (count = 0; count < 25; count++)
             {
-                Console.Write("Please enter in a number: ");
-                int.TryParse(Console.ReadLine(), out value);
-                if (value != 0 && count < 25)
+                //  prompt for a number
+                Console.Write("Please enter in a number (0 to quit): ");
+
+                //  get the number the user input
+                int.TryParse(Console.ReadLine(), out inputValue);
+                //  check if the user has enter a zero to quit
+                if (inputValue != 0)
                 {
-                    values[count] = value;
-                    count += 1;
+                    //  store the number in the current count position
+                    numbers[count] = inputValue;
                 }
                 else
                 {
-                    cont = false;
+                    //  exit the loop
+                    break;
                 }
-            } while (cont);
-
-            foreach (var i in values)
-            {
-                total = total + i;
             }
-            Console.WriteLine($"You enter {count} values and the average is {total / count}");
+
+
+            //  iterate the list 
+            /*
+            //  using the for loop
+            for (int i = 0; i < count; i++)
+            {
+                //  sum all the number enter
+                total = total + numbers[i];
+            }
+            */
+            foreach (var number in numbers)
+            {
+                //  sum all the number enter
+                total = total + number;
+            }
+
+            if (count > 0)
+            {
+                average = total / count;
+            }
+            Console.WriteLine($"You enter {count} values and the average is {average}");
         }
     }
 }
