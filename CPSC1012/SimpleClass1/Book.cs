15c037866e0dde38c62fbb86705678c54f493169
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2025-04-01 07:06:25 -0600

    End of class

---
+++ CPSC1012/SimpleClass1/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace W12D1_SimpleClass
+{
+    public class Book
+    {        
+        #region Properties
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        #endregion
+    }
+}
---
+++ CPSC1012/SimpleClass1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace W12D1_SimpleClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  multiple ways of creating a class
+            //  Old way
+            // Book book = new Book();
+            Book book = new();
+
+            // YOU WILL LOSE MARKS
+            Book badBookWay = new Book();
+
+            book.Title = "James Best Coding Examples";
+
+            Console.WriteLine($"Book Tile: {book.Title}");
+
+        }
+    }
+}
